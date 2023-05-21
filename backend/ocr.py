e20916808e4822a2614fa0a35cd819ad987d720c
Akash Gupta <asgupta2141@gmail.com>
2023-05-21 17:43:57 +0530

    uncommented the code

--- backend/ocr.py
+++ backend/ocr.py
@@ -21,13 +21,13 @@ def extract_text(filepath):
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    predicted_str = ''
+    predicted_str = 'test'
     h_list = [];
     contours = sort_contours(contours)[1:]
-    # for cnt in contours:
-    #     x, y, w, h = cv2.boundingRect(cnt)
-    #     if w * h > 290:
-    #         h_list.append([x, y, w, h])
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if w * h > 290:
+            h_list.append([x, y, w, h])
     # for hl in h_list:
     #     x, y, w, h = hl
     #     test_image = gray[y:y + h, x:x + w]
@@ -42,8 +42,7 @@ def extract_text(filepath):
     #     rs = arr_result[index[1][0]]
     #     predicted_str = predicted_str + rs
     # os.remove(filepath)
-    # return predicted_str
-    return filepath
+    return predicted_str
 
 # def show_img(img,name="image"):
 #     if(DEBUG_MODE):
